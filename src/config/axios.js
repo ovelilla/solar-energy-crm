d2c6340cb76edfa5eb86a9c39066475c9e7f111b
Oscar <o.velillaa@gmail.com>
2023-01-23 06:19:21 +0100

    updates

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,15 @@
+// import axios from "axios";
+
+// const axiosClient = axios.create({
+//     baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+// });
+
+// export default axiosClient;
+
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+    baseURL: "https://solar-energy-backend.vercel.app/api",
 });
 
 export default axiosClient;
